cd8e267a9651f81a4b3b2a805f844624a17e71da
Dini Rahmadani <dini@lazuardi.sch.id>
2025-11-12 15:51:40 +0700

    initial commit: lazgo-lazelo app

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,6 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         }
       },
-     base: process.env.NODE_ENV === 'production'?'/lazgo/':'/',
+     base: process.env.NODE_ENV === 'production'?'/lazgo-lazelo/':'/',
     };
 });
